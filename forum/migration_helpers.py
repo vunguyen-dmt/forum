77a627296421348efd58f76cd493e868d0c7c467
vunguyen-dmt <34919039+vunguyen-dmt@users.noreply.github.com>
2025-04-11 21:50:14 +0700

    Update migration_helpers.py

--- forum/migration_helpers.py
+++ forum/migration_helpers.py
@@ -25,7 +25,27 @@ from forum.models import (
 from forum.utils import make_aware, get_trunc_title
 
 def replace_emojis(text, replacement="[EMOJI]"):
-    return text.encode("utf-8", errors="replace").decode("utf-8")
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, replacement, text)
 
 def get_all_course_ids(db: Database[dict[str, Any]]) -> list[str]:
     """Get all course IDs from MongoDB."""
